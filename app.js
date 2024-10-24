8435fabe4173de4ac6c2fff1f79ad04529275a32
”aranzazuDAA” <aran_dorado@hotmail.com>
2024-10-24 20:29:17 +0200

    initial

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+
+const books = require('./routes/books');
+const authors = require('./routes/authors'); 
+
+app.use('/books', books);
+app.use('/authors', authors);
+
+app.use('/*', (req, res) => {
+    res.status(404).json({
+        message: 'Incorrect route or params',
+    })
+})
+
+app.listen(port, () => {
+ console.log(`Servidor escuchando en http://localhost:${port}`);
+})
+
+module.exports = app;
